2c863c716577d6ff32bf5cfd3fd5fdf6081bd23d
Emil <Emilknudsen1@live.dk>
2021-03-19 12:22:24 +0100

    add A test on plan CTA

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -6,9 +6,7 @@ function CarouselItem({ image, copy, ctaLink, ctaCopy }) {
       <img src={image} alt="" className="carouselItem__image"/>
       <div className="carouselItem__content">
         <p className="carouselItem__copy">{copy}</p>
-        <a href={ctaLink} className="carouselItem__CTA" 
-        // eslint-disable-next-line
-        onClick={() => gtag("event", "select_content", { "content_type": ctaLink })}>{ctaCopy}</a>
+        <a href={ctaLink} className="carouselItem__CTA">{ctaCopy}</a>
       </div>
     </div>
   )
